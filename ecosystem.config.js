61b2630015a428ba531eae99609a02e5dcd12560
Kemal Dwiheldy <33623566+SwiftDSG@users.noreply.github.com>
2023-01-09 06:40:11 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'overhous-admin',
+      name: 'quicks',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs',
       env: {
-        "NUXT_APP_BASE_URL": "/overhous",
+        "NUXT_APP_BASE_URL": "/quicks",
         "PORT": 3005
       }
     }
